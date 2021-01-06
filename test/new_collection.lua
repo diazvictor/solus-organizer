58016404f5c6ba543392a34a51eae1ac18637128
diazvictor <diaz.victor@openmailbox.org>
2021-01-06 02:35:15 -0400

    page status, data validation and data saving

--- test/new_collection.lua
+++ test/new_collection.lua
@@ -12,13 +12,14 @@ clear_collection_info = function ()
 	ui.textbuffer_launch_collection.text = ''
 end
 
-update_collection_info = function ()
-	ui.entry_name_collection.text = metadata.collection[currentCollection]['name']
-	ui.entry_shortened_collection.text = metadata.collection[currentCollection]['shortened']
-	ui.textbuffer_launch_collection.text = metadata.collection[currentCollection]['launch']
+update_collection_info = function (collectionName)
+	ui.entry_name_collection.text = metadata.collection[collectionName]['name']
+	ui.entry_shortened_collection.text = metadata.collection[collectionName]['shortened']
+	ui.textbuffer_launch_collection.text = metadata.collection[collectionName]['launch']
+	route = 'edit_collection'
 end
 
-set_collection_info = function ()
+edit_collection_info = function ()
 	metadata.collection[currentCollection]['name'] = ui.entry_name_collection.text
 	metadata.collection[currentCollection]['shortened'] = ui.entry_shortened_collection.text
 	metadata.collection[currentCollection]['launch'] = ui.textbuffer_launch_collection.text
@@ -28,29 +29,55 @@ set_collection_info = function ()
 	ui.header.title = 'Solus Frontend - New Game'
 end
 
--- @TODO: validando los campos, si no hay campos cambiados cambio a la siguiente pagina
--- sino guardo los cambios y cambiao a la siguiente pagina
+set_collection_info = function ()
+	metadata.collection[ui.entry_shortened_collection.text] = {
+		name = ui.entry_name_collection.text,
+		shortened = ui.entry_shortened_collection.text,
+		launch = ui.textbuffer_launch_collection.text,
+		files = {}
+	}
+	ui.section:set_visible_child_name('page_new_game')
+	route = 'new_game'
+	ui.header.title = 'Solus Frontend - New Game'
+end
+
 validate_collection_info = function ()
-	if ui.entry_name_collection.text == metadata.collection[currentCollection]['name']
-		and ui.entry_shortened_collection.text == metadata.collection[currentCollection]['shortened']
-		and ui.textbuffer_launch_collection.text == metadata.collection[currentCollection]['launch']
-	then
-		return false, 'ERROR: las entradas son iguales, cambio a la siguiente pagina!'
-	elseif ui.entry_name_collection.text ~= metadata.collection[currentCollection]['name']
-		or ui.entry_shortened_collection.text ~= metadata.collection[currentCollection]['shortened']
-		or ui.textbuffer_launch_collection.text ~= metadata.collection[currentCollection]['launch']
-	then
-		return true, 'Guardado correctamente!'
+	if route == 'edit_collection' then
+		if ui.entry_name_collection.text == metadata.collection[currentCollection]['name']
+			and ui.entry_shortened_collection.text == metadata.collection[currentCollection]['shortened']
+			and ui.textbuffer_launch_collection.text == metadata.collection[currentCollection]['launch']
+		then
+			return false, 'ERROR: las entradas son iguales, cambio a la siguiente pagina!'
+		elseif ui.entry_name_collection.text ~= metadata.collection[currentCollection]['name']
+			or ui.entry_shortened_collection.text ~= metadata.collection[currentCollection]['shortened']
+			or ui.textbuffer_launch_collection.text ~= metadata.collection[currentCollection]['launch']
+		then
+			return true, 'Guardado correctamente!'
+		end
+	elseif route == 'new_collection' then
+		if ui.entry_name_collection.text ~= ''
+			and ui.entry_shortened_collection.text ~= ''
+			and ui.textbuffer_launch_collection.text ~= ''
+		then
+			return true, 'Guardado correctamente!'
+		else
+			return false, 'Fallo al guardar!'
+		end
 	end
 end
 
 save_collection_info = function ()
 	local ok, msg = validate_collection_info()
 	if ok then
-		set_collection_info()
+		if route == 'edit_collection' then
+			edit_collection_info()
+			update_collection_info(currentCollection)
+			update_collection_name()
+		elseif route == 'new_collection' then
+			set_collection_info()
+		end
 		config:save(configdir, metadata)
-		update_collection_name()
-		update_collection_info()
+		populate_collection()
 	else
 		ui.section:set_visible_child_name('page_new_game')
 		route = 'new_game'
@@ -65,12 +92,14 @@ end
 
 -- ir a page_new_collection
 function ui.btn_new_info:on_clicked()
-	update_collection_info()
 	ui.section:set_visible_child_name('page_new_collection')
+	route = 'new_collection'
+	currentCollection = ''
 end
 
 -- volver a page_list
 ui.btn_back_page_list['on_clicked'] = function ()
 	ui.section:set_visible_child_name('page_list')
+	clear_collection_info()
 	populate_gamelist()
 end
