97baa79b63d39bac845c7a517ce74783d5ce446e
diazvictor <diaz.victor@openmailbox.org>
2021-01-06 00:02:30 -0400

    testing

--- test/parser.lua
+++ test/parser.lua
@@ -1,10 +1,24 @@
-package.path = package.path .. ';../?.solus.txt'
+-- package.path = package.path .. ';../?.solus.txt'
 
-local metadata = {}
-metadata.gba = require 'metadata'
+-- metadata.gba = require 'metadata'
+metadata = {
+	collection = {
+		gba = {
+			name = "GAME BOY ADVANCE",
+			shortened = "gba",
+			launch = "/home/victor/Proyectos/GIT/mgba/sdl/mgba",
+			files = {
+				{},
+				{}
+			}
+		}
+	}
+}
+print(#metadata['collection']['gba']['files'])
+-- print(id_row)
 
-for i, v in pairs(metadata.gba) do
-    print(i, v)
-end
+-- for i, v in pairs(metadata.gba) do
+    -- print(i, v)
+-- end
 
-return config
+-- return config
