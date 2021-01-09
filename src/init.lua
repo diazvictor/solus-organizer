4420f7e0a6f90373e5bda8f89428673f16b9e5df
diazvictor <diaz.victor@openmailbox.org>
2021-01-09 13:17:48 -0400

    simple log, titulo de ventana y botones al final del headerbar

--- src/init.lua
+++ src/init.lua
@@ -3,35 +3,52 @@
  @filename  init.lua
  @version   1.0
  @author    Díaz Urbaneja Víctor Eduardo Diex <victor.vector008@gmail.com>
- @date      21.07.2020 20:01:41 -04
+ @date      07.01.2021 19:33:02 -04
 ]]
-package.path = package.path .. ';../?.lua'
 
-require 'lib.middleclass'
-json = require 'lib.json'
-utils = require 'lib.utils'
-config = require 'lib.configuration'
+package.path = package.path .. ';../lib/?.lua'
 
+print("Loading libraries:")
 lgi = require 'lgi'
+print("| 'lgi' loaded successfully. (Thanks Pavouk!)")
 
 GObject = lgi.GObject
-GLib = lgi.GLib
 Gtk = lgi.require('Gtk', '3.0')
+Gdk = lgi.Gdk
+GLib = lgi.GLib
 GdkPixbuf = lgi.GdkPixbuf
+
+require 'solus'
+print("Libraries loaded!\n")
 
 assert = lgi.assert
 local builder = Gtk.Builder()
-
-assert(builder:add_from_file('main_window.ui'), "ERROR: el archivo no existe")
-
+assert(builder:add_from_file('../data/gtk/main_window.ui'), 'ERROR: the file does not exist')
 ui = builder.objects
 
-require('test.stack') -- esto es una prueba
-require('test.new_collection')
-require('test.edit_game')
-require('test.new_game')
+ui.main_window.title = 'Solus Frontend'
+
+ui.head:pack_end(Gtk.Box {
+	orientation = 'HORIZONTAL',
+	spacing = 6,
+	Gtk.Button {
+		on_clicked = function ()
+			print('Click!')
+		end
+	},
+	Gtk.Separator {},
+	Gtk.MenuButton {
+		id = 'btn_menu',
+		Gtk.Image {
+			icon_name = 'open-menu-symbolic'
+		}
+	}
+}, false, false, 0)
+
+ui.head.child.btn_menu:set_popover(ui.menu)
 
 function ui.main_window:on_destroy()
+	db:close()
 	Gtk.main_quit()
 end
 
