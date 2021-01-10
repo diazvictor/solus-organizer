cd1ffe02e07e6eeaa0d9311c6f2eeb8fba8ea2f3
diazvictor <diaz.victor@openmailbox.org>
2021-01-09 23:44:09 -0400

    Show game info and go to "gameinfo"

--- src/gameinfo.lua
+++ src/gameinfo.lua
@@ -6,6 +6,37 @@
  @date      09.01.2021 17:14:18 -04
 ]]
 
+--- Get the info game
+-- @param id number: id of game
+-- @return table: the info
+solus['get_game_info'] = function (id)
+	local sql = [[
+		select games.id_game, games.name, games.rom, collections.launch
+		from collections
+		join games on (games.id_collection = collections.id_collection)
+		where games.id_game = %d;
+	]]
+	local game = db:get_rows(sql, id)
+	return game[1]
+end
+
+--- Show game info and go to "gameinfo"
+-- @return boolean: true
+solus['show_game_info'] = function (id)
+	local game = solus.get_game_info(id)
+	local command = ('"%s" "%s" &'):format(game.launch, game.rom)
+
+	ui.gameinfo_title.label = game.name
+	ui.gameinfo_desc.label = game.description or 'null'
+
+	ui.gameinfo_btn_launch['on_clicked'] = function ()
+		os.execute(command)
+	end
+
+	ui.section:set_visible_child_name('gameinfo')
+	return true
+end
+
 ui.gameinfo_sidebar['on_row_activated'] = function ()
 	if ui.btn_info_desc:is_selected() == true then
 		ui.gameinfo_pages:set_visible_child_name('gameinfo_page_desc')
