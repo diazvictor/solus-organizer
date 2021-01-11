952c9a9c529abf4da832ce77129523e5e847c3cd
diazvictor <diaz.victor@openmailbox.org>
2021-01-11 03:48:07 -0400

    show logo or name

--- src/gamelist.lua
+++ src/gamelist.lua
@@ -12,20 +12,42 @@
 solus['show_games'] = function (id_collection)
 	-- get info of game
 	local sql = [[
-		select games.id_collection,id_game,
-		games.name,games.rom
+		select id_collection, id_game, logo, name, rom
 			from games
-		where id_collection = %d order by games.id_game asc
+		where id_collection = %d
 	]]
 	local games = db:get_rows(sql, id_collection)
+
+	local cover = nil
 	for _, game in pairs(games) do
-		ui.gamelist.child[game.id_collection]:insert(Gtk.FlowBoxChild {
-			id = game.id_game,
-			width = 200,
-			height = 100,
-			Gtk.Label {
+		if game.logo ~= nil and game.logo ~= '' then
+			local image_decode = base64.decode(game.logo)
+			local stream = Gio.MemoryInputStream.new_from_data(image_decode)
+			local image = GdkPixbuf.Pixbuf.new_from_stream(stream)
+			image = image:scale_simple(200, 150, 'BILINEAR')
+
+			cover = Gtk.Overlay {
+				id = 'overlay' .. game.id_game,
+				visible = true,
+				Gtk.Image {
+					visible = true,
+					pixbuf = image
+				}
+			}
+			print(utils:truncate(game.name, 17) .. ' logo loading ... ', image)
+		else
+			cover = Gtk.Label {
+				visible = true,
 				label = utils:truncate(game.name, 21)
 			}
+			print(utils:truncate(game.name, 17) .. ' logo not found ...', image)
+		end
+		ui.gamelist.child[game.id_collection]:insert(Gtk.FlowBoxChild {
+			id = game.id_game,
+			visible = true,
+			width = 200,
+			height = 200,
+			cover
 		}, game.id_game)
 	end
 end
