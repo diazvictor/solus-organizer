bcb8e9817bbe6f30a8c3f9d603fc533208d83f3b
diazvictor <diaz.victor@openmailbox.org>
2021-01-11 17:35:28 -0400

    show a collection by id

--- src/collections.lua
+++ src/collections.lua
@@ -6,6 +6,7 @@
  @date      10.01.2021 03:41:53 -04
 ]]
 
+--- Show all collections
 solus['show_collections'] = function ()
 	-- get info of game
 	local sql = [[
@@ -40,4 +41,42 @@ solus['show_collections'] = function ()
 	end
 end
 
-solus.show_collections()
+--- Show a collection by id
+-- @param id_collection number: collection id
+solus['show_collection'] = function (id_collection)
+	-- get info of game
+	local sql = [[
+		select id_collection,logo,shortname
+			from collections
+		where id_collection = %d
+	]]
+	local collection = db:get_rows(sql, id_collection)
+	local ok = ui.collection_logo:get_child_by_name(id_collection) -- check collection
+
+	if not ok then
+		local image_decode = base64.decode(collection[1]['logo'])
+		local stream = Gio.MemoryInputStream.new_from_data(image_decode)
+		local image	= GdkPixbuf.Pixbuf.new_from_stream(stream)
+
+		ui.collection_logo:add_titled(Gtk.Image {
+			visible = true,
+			pixbuf = image
+		}, collection[1]['id_collection'], collection[1]['shortname'])
+
+		ui.gamelist:add_titled(Gtk.FlowBox {
+			id = collection[1]['id_collection'],
+			visible = true,
+			column_spacing = 20,
+			row_spacing = 20,
+			max_children_per_line = 4,
+			min_children_per_line = 2,
+			activate_on_single_click = false,
+			on_child_activated = function (self, flowboxchild)
+				solus.show_game_info(flowboxchild.id)
+			end
+		}, collection[1]['id_collection'], collection[1]['shortname'])
+		solus.show_games(collection[1]['id_collection'])
+	end
+	ui.collection_logo:set_visible_child_name(collection[1]['id_collection'])
+	ui.gamelist:set_visible_child_name(collection[1]['id_collection'])
+end
